b0f7a899a49da5ddd9f0c56801572520faab7159
wallace moreira da silva <wallaceinfofuturo@gmail.com>
2021-06-06 13:13:37 -0300

    Criando classe dos metodos e teste

---
+++ Calculos/CalculosTeste/CalculoUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using Calculos;
+using Xunit;
+
+namespace CalculosTeste
+{
+    public class CalculoUnitTest
+    {
+        [Fact]
+        public void Somar_DoisDouble_RetornarDouble()
+        {
+            //Arrange
+            var num1 = 2.9;
+            var num2 = 3.1;
+            var valorEsperado = 6;
+
+            //Act
+            var soma = Calculo.Somar(num1, num2);
+
+            //Assert
+            Assert.Equal(valorEsperado, soma) ;
+        }
+    }
+}
