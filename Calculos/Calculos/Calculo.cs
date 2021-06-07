6b814660bd709f2b79e648bd67dc1cb524086a30
wallace moreira da silva <wallaceinfofuturo@gmail.com>
2021-06-06 22:00:10 -0300

    Ajuste nas classes

--- Calculos/Calculos/Calculo.cs
+++ Calculos/Calculos/Calculo.cs
@@ -4,20 +4,56 @@
     {
         public static double Somar(double numero1, double numero2)
         {
-            return (numero1 + numero2);
+            try
+            {
+                return (numero1 + numero2);
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
         }
+
         public static double Subtrair(double numero1, double numero2)
         {
-            return (numero1 - numero2);
+            try
+            {
+                return (numero1 - numero2);
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
         }
+
         public static double Multiplicar(double numero1, double numero2)
         {
-            return (numero1 * numero2);
+            try
+            {
+                return (numero1 * numero2);
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
         }
+
         public static double Dividir(double numero1, double numero2)
         {
-            return (numero1 / numero2);
+            try
+            {
+                return (numero1 / numero2);
+            }
+            catch (System.DivideByZeroException)
+            {
+                return -999;
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
         }
+
         public static bool IsNumeroPar(int numero)
         {
             return numero % 2 == 0;
